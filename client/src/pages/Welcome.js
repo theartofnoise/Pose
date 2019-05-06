df9c45579480aeb263469c20cd9786e523c1a1bc
Art G <theartofnoise@gmail.com>
2019-05-06 11:22:14 -0400

    feat: login form

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -9,16 +9,41 @@ import { Input, TextArea, FormBtn } from "../components/Form";
 
 class Welcome extends Component {
   state = {
-    
+        userEmail:"",
+        userPassword:"",
   };
 
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    alert("Submitted");
+    console.log(this.state);
+  };
 
   render() {
     return (
       <Container fluid>
         <Row>
           <Col size="md-6">
-            whasssup
+            <form>
+                <Input 
+                    value={this.state.userEmail}
+                    onChange={this.handleInputChange}
+                    type="email" 
+                    placeholder="email" />
+                <Input 
+                    value={this.state.userPassword}
+                    onChange={this.handleInputChange}
+                    type="password" 
+                    placeholder="password" />
+                <FormBtn type="submit">Submit</FormBtn>
+            </form>
           </Col>
         </Row>
       </Container>
