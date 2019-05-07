a05c94399e5b9b3eca0ce0e0f4a3f6071850c5df
Art G <theartofnoise@gmail.com>
2019-05-07 01:33:15 -0400

    disabled links untill logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Welcome from "./pages/Welcome";
 import Main from "./pages/Main";
 import UserProjects from "./pages/UserProjects";
+import Nav from "../src/components/"
 
-function App() {
+class App extends Component {
+
+  state = {
+    loggedIn:true,
+  }
+
+  render() {
   return (
+    <div>
+    <Nav />
     <Router>
-      <div>
         <Switch>
           <Route exact path="/" component={Welcome} />
-          <Route exact path="/main" component={Main} />
-          <Route exact path="/userProjects" component={UserProjects} />
-          <Route exact path="/books/:id" component={Detail} />
+          {this.state.loggedIn&&[
+          <Route exact path="/main" component={Main} />,
+          <Route exact path="/userProjects" component={UserProjects} />,
+          <Route exact path="/books/:id" component={Detail} />]}
           <Route component={NoMatch} />
         </Switch>
-      </div>
     </Router>
+      </div>
   );
 }
-
+}
 export default App;
